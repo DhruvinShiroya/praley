6dba23fc559b9a542083efbe3b907046094365ea
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-08-09 22:04:56 -0400

    fix for the #if is added

--- model/message.js
+++ model/message.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 // use plm package to extent the user model
 
 var userSchemaDefinition = {
+    sender : {type}
   username: { type: String, required: true },
   password: { type: String },
 };
 
 var userSchema = new mongoose.Schema(userSchemaDefinition);
 
-module.exports = new mongoose.model("User", userSchema);
+module.exports = new mongoose.model("message", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Chat application" });
 });
 
+// logout and clear the session
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    res.redirect("users/login");
+  });
+});
 module.exports = router;
